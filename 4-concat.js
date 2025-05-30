ab514da4b97e0e0390e3fce6148810b1c7ceedf9
Muhammad Ali <muhe098@gmail.com>
2025-05-30 17:03:21 +0300

    Add 4-concat.js

---
+++ 4-concat.js
@@ -0,0 +1,3 @@
+const arg1 = process.argv[2]; // get the first user argument
+const arg2 = process.argv[3]; // get the second user argument
+console.log(`${arg1} is ${arg2}`); // Print the first and second arguments
